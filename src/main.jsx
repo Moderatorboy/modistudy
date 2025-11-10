dbd0bc1f4178862b5d6f9ea7602aa13062981073
Moderatorboy <bitturaj845418@gmail.com>
2025-11-11 01:09:08 +0530

    Updated images, main.jsx and added ChapterList component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+
+// Global styles
 import './index.css'
+import './styles/theme.css'   // 👈 ye naya line add karo
 
 createRoot(document.getElementById('root')).render(<App />)
---
+++ src/styles/ChapterList.jsx
@@ -0,0 +1,4 @@
+<div className="card">
+  <h2>Electrostatics</h2>
+  <button className="btn">Watch Lecture</button>
+</div>
