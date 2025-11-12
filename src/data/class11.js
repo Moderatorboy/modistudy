d9f70bb24732d7e1d431ef4d3d54eb4a093ff8fc
Moderatorboy <bitturaj845418@gmail.com>
2025-11-12 14:26:22 +0530

    Initial commit

---
+++ src/data/class11.js
@@ -0,0 +1,72 @@
+export const class11 = {
+  id: "class11",
+  name: "Class 11th",
+  subjects: [
+    {
+      id: "phy11",
+      name: "PHYSICS (NS SIR)",
+      chapters: [
+        {
+          id: "c11_1",
+          name: "UNITS & DIMENSIONS",
+          lectures: [
+            { id: "NS_UAD_L01", title: "Lecture 01", url: "https://rumble.com/embed/NS_UAD_L01" },
+            { id: "NS_UAD_L02", title: "Lecture 02", url: "https://rumble.com/embed/NS_UAD_L02" }
+          ]
+        },
+        {
+          id: "c11_2",
+          name: "KINEMATICS",
+          lectures: [
+            { id: "NS_KIN_L01", title: "Lecture 01", url: "https://rumble.com/embed/NS_KIN_L01" },
+            { id: "NS_KIN_L02", title: "Lecture 02", url: "https://rumble.com/embed/NS_KIN_L02" }
+          ]
+        }
+      ]
+    },
+    {
+      id: "chem11",
+      name: "CHEMISTRY (AKK SIR)",
+      chapters: [
+        {
+          id: "c11_3",
+          name: "MOLE CONCEPT",
+          lectures: [
+            { id: "AKK_MC_L01", title: "Lecture 01", url: "https://rumble.com/embed/AKK_MC_L01" },
+            { id: "AKK_MC_L02", title: "Lecture 02", url: "https://rumble.com/embed/AKK_MC_L02" }
+          ]
+        },
+        {
+          id: "c11_4",
+          name: "ATOMIC STRUCTURE",
+          lectures: [
+            { id: "AKK_AS_L01", title: "Lecture 01", url: "https://rumble.com/embed/AKK_AS_L01" },
+            { id: "AKK_AS_L02", title: "Lecture 02", url: "https://rumble.com/embed/AKK_AS_L02" }
+          ]
+        }
+      ]
+    },
+    {
+      id: "math11",
+      name: "MATHEMATICS (VG SIR)",
+      chapters: [
+        {
+          id: "c11_5",
+          name: "FUNCTIONS",
+          lectures: [
+            { id: "VG_FUNC_L01", title: "Lecture 01", url: "https://rumble.com/embed/VG_FUNC_L01" },
+            { id: "VG_FUNC_L02", title: "Lecture 02", url: "https://rumble.com/embed/VG_FUNC_L02" }
+          ]
+        },
+        {
+          id: "c11_6",
+          name: "LIMITS",
+          lectures: [
+            { id: "VG_LIMIT_L01", title: "Lecture 01", url: "https://rumble.com/embed/VG_LIMIT_L01" },
+            { id: "VG_LIMIT_L02", title: "Lecture 02", url: "https://rumble.com/embed/VG_LIMIT_L02" }
+          ]
+        }
+      ]
+    }
+  ]
+};
---
+++ src/data/class12.js
@@ -0,0 +1,72 @@
+export const class12 = {
+  id: "class12",
+  name: "Class 12th",
+  subjects: [
+    {
+      id: "phy12",
+      name: "PHYSICS (JP SIR)",
+      chapters: [
+        {
+          id: "c12_1",
+          name: "ELECTROSTATICS",
+          lectures: [
+            { id: "JP_ES_L01", title: "Lecture 01", url: "https://rumble.com/embed/JP_ES_L01" },
+            { id: "JP_ES_L02", title: "Lecture 02", url: "https://rumble.com/embed/JP_ES_L02" }
+          ]
+        },
+        {
+          id: "c12_2",
+          name: "CURRENT ELECTRICITY",
+          lectures: [
+            { id: "JP_CE_L01", title: "Lecture 01", url: "https://rumble.com/embed/JP_CE_L01" },
+            { id: "JP_CE_L02", title: "Lecture 02", url: "https://rumble.com/embed/JP_CE_L02" }
+          ]
+        }
+      ]
+    },
+    {
+      id: "chem12",
+      name: "CHEMISTRY (AKK SIR)",
+      chapters: [
+        {
+          id: "c12_3",
+          name: "SOLID STATE",
+          lectures: [
+            { id: "AKK_SS_L01", title: "Lecture 01", url: "https://rumble.com/embed/AKK_SS_L01" },
+            { id: "AKK_SS_L02", title: "Lecture 02", url: "https://rumble.com/embed/AKK_SS_L02" }
+          ]
+        },
+        {
+          id: "c12_4",
+          name: "CHEMICAL KINETICS",
+          lectures: [
+            { id: "AKK_CK_L01", title: "Lecture 01", url: "https://rumble.com/embed/AKK_CK_L01" },
+            { id: "AKK_CK_L02", title: "Lecture 02", url: "https://rumble.com/embed/AKK_CK_L02" }
+          ]
+        }
+      ]
+    },
+    {
+      id: "math12",
+      name: "MATHEMATICS (GB SIR)",
+      chapters: [
+        {
+          id: "c12_5",
+          name: "INVERSE TRIGONOMETRIC FUNCTION",
+          lectures: [
+            { id: "GB_ITF_L01", title: "Lecture 01", url: "https://rumble.com/embed/GB_ITF_L01" },
+            { id: "GB_ITF_L02", title: "Lecture 02", url: "https://rumble.com/embed/GB_ITF_L02" }
+          ]
+        },
+        {
+          id: "c12_6",
+          name: "CONTINUITY",
+          lectures: [
+            { id: "GB_CONT_L01", title: "Lecture 01", url: "https://rumble.com/embed/GB_CONT_L01" },
+            { id: "GB_CONT_L02", title: "Lecture 02", url: "https://rumble.com/embed/GB_CONT_L02" }
+          ]
+        }
+      ]
+    }
+  ]
+};
