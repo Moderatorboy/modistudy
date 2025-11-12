f135b5c40cd05777e1f54881a9f121de699ec6b9
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2025-11-12 17:41:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import "./styles/theme.css";
 import { class11 } from './data/class11';
 import { class12 } from './data/class12';
+import "./styles/theme.css";
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(true);
